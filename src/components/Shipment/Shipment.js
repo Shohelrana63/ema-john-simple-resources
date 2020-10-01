ebd5899ff5333b9cffcb3f28957dc5093e528b0d
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2020-10-02 00:40:26 +0600

    save order in a new data collection in mongodb

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -2,41 +2,60 @@ import React from 'react';
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { UserContext } from '../../App';
+import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
 import './Shipment.css';
 
 const Shipment = () => {
-    const { register, handleSubmit, watch, errors } = useForm();
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const onSubmit = data => {
-    console.log('form submitted', data)
+  const { register, handleSubmit, watch, errors } = useForm();
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const onSubmit = data => {
+    const savedCart = getDatabaseCart();
+    const orderDetails = { ...loggedInUser, products: savedCart, shipment: data, orderTime: new Date() }
+
+    fetch('http://localhost:5000/addOrder', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(orderDetails)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data) {
+          processOrder();
+          alert('your order is successfully')
+        }
+      })
+
+
   };
 
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-   
+
     <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
- 
-    
+
+
       <input name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} placeholder="Your Name" />
       {errors.name && <span className="error">Name is required</span>}
-     
+
       <input name="email" defaultValue={loggedInUser.email} ref={register({ required: true })} placeholder="Your Email" />
       {errors.email && <span className="error">Email is required</span>}
-     
-      <input name="address" ref={register({ required: true })} placeholder="Your Address"/>
+
+      <input name="address" ref={register({ required: true })} placeholder="Your Address" />
       {errors.address && <span className="error">Address is required</span>}
-     
-      <input name="phone" ref={register({ required: true })} placeholder="Your Phone Number"/>
+
+      <input name="phone" ref={register({ required: true })} placeholder="Your Phone Number" />
       {errors.phone && <span className="error">Phone Number is required</span>}
 
-      <input name="city" ref={register({ required: true })} placeholder="Your City"/>
+      <input name="city" ref={register({ required: true })} placeholder="Your City" />
       {errors.phone && <span className="error">City is required</span>}
 
-      <input name="zip" ref={register({ required: true })} placeholder="Zip Code"/>
+      <input name="zip" ref={register({ required: true })} placeholder="Zip Code" />
       {errors.phone && <span className="error">Zip Code is required</span>}
 
-      
+
       <input type="submit" />
     </form>
   );
